41ef3b2eaae0850c140b96b58c690b743f34e804
rhyde23 <reginaldjhyde@gmail.com>
2024-06-05 09:14:29 -0500

    Update lookForOldComputers.py

--- lookForOldComputers.py
+++ lookForOldComputers.py
@@ -6,39 +6,70 @@ import openpyxl
 #datetime - Library for working with dates
 from datetime import date
 
-#ldap3 - Library for connecting to AD
-from ldap3 import Connection
+#pyad - Library for connecting to AD
+from pyad import *
 
 #Function to prompt the user for desired settings
 def prompt_user() :
+
+    #Prompt user for the Excel file path
     workbook_path = input("Enter the file path of the excel workbook containing the Blubeam Computer data >> ")
     print()
+
+    #Prompt user for the last ping column name in the Excel file
     last_ping_column_name = input("Enter the header name of the column that contains the Last Ping dates >> ")
     print()
 
+    #Loop until user inputs valid integer
     while True :
+
+        #Try to convert input into integer
         try :
+
+            #Prompt user for the maximum amount of days old since last ping
             max_days_old = int(input("Enter the maximum days since last ping >> "))
             print()
+
+            #Break loop if no exception
+            break
+
+        #If exception, print error message
         except :
             print()
             print("Please enter an integer.")
             print()
+    
+    #Prompt user for the name of the LDAP server
+    ldap_server = input("Enter the name of your ldap server >> ")
+    print()
 
-    ip_address = input("Enter your ")
+    #Prompt user for username
+    admin_user = input("Enter your admin username >> ")
+    print()
+
+    #Prompt user for password
+    admin_passwd = input("Enter your admin password >> ")
+    print()
+
+    #Call "identify_old_computers"
+    identify_old_computers(max_days_old, workbook_path, last_ping_column_name, ldap_server, admin_user, admin_passwd)
+    
     
             
 #Function to identify the old Blubeam computers from an Excel spreadsheet
-def identify_old_computers(max_days_old, workbook_path, last_ping_column_name) :
+def identify_old_computers(max_days_old, workbook_path, last_ping_column_name, ldap_server, admin_user, admin_passwd) :
 
     #Load Excel sheet 
     workbook = openpyxl.load_workbook(workbook_path)
 
     #Get the active sheet
-    sheet = workbook.active 
+    sheet = workbook.active
+
+    #Connect to AD with pyad
+    pyad.set_defaults(ldap_server= ldap_server, username=admin_user, password=admin_passwd)
 
     #Print message
-    print("Here are the computers older than "+str(max_days_old)+" days old.")
+    print("Here are the computers older than "+str(max_days_old)+" days old that are not in AD: ")
     print()
 
     #Iterate through each column
@@ -74,6 +105,14 @@ def identify_old_computers(max_days_old, workbook_path, last_ping_column_name) :
                         #If the first three characters of the cell's value is GRE, it is the device name
                         if cell.value[:3] == "GRE" :
 
-                            #Print the device name
-                            print(cell.value)
+                            #Search for device name in AD
+                            try :
+                                computer_search = pyad.adcomputer.ADComputer.from_cn(cell.value)
 
+                            #If not in AD, print device name
+                            except :
+                                
+                                #Print the device name
+                                print(cell.value)
+
+prompt_user()
